8f4a2c3e4a8630be5302e0d086a4d8f0aa91cf5d
Well-Behaved Demon <wellbehaveddemon@proton.me>
2026-01-09 17:52:23 -0300

    test: check if AnonymityRouter.createServer works as intended

--- tests.mjs
+++ tests.mjs
@@ -1,3 +1,4 @@
 import './modules/cryptography/elgamal/tests.mjs'
 import './modules/cryptography/twofish/tests.mjs'
 import './modules/packets/coordination/format/tests.mjs';
+import './modules/router/tests.mjs';
