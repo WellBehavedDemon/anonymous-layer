0045c47774a95dfe5009c95e827a1488538e7b19
Well-Behaved Demon <wellbehaveddemon@proton.me>
2026-01-22 22:18:40 -0300

    fix: add test leaf to the test tree

--- tests.mjs
+++ tests.mjs
@@ -2,4 +2,5 @@ import './modules/cryptography/elgamal/tests.mjs'
 import './modules/cryptography/twofish/tests.mjs'
 import './modules/packets/coordination/format/tests.mjs';
 import './modules/packets/coordination/parse/tests.mjs';
+import './modules/random/tests.mjs';
 import './modules/router/tests.mjs';
