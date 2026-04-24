69c0f7bddc6b9dc626a6d93a0a1ad2a527b8070c
Well-Behaved Demon <wellbehaveddemon@proton.me>
2026-04-24 17:05:18 -0300

    fix: add tests to root test file

--- tests.mjs
+++ tests.mjs
@@ -4,5 +4,6 @@ import './modules/cryptography/twofish/tests.mjs';
 import './modules/packets/coordination/format/tests.mjs';
 import './modules/packets/coordination/parse/tests.mjs';
 import './modules/random/global/tests.mjs';
+import './modules/random/local/tests.mjs';
 import './modules/router/tests.mjs';
 import './modules/utilities/tests.mjs';
